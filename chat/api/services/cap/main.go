2a77e2cfed490560da031b2f145b6aef86c05724
cod3ddy <edwardchirwa07@gmail.com>
2025-02-17 02:27:10 +0200

    chore: service/cap

---
+++ chat/api/services/cap/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"runtime"
+
+	"github.com/Cod3ddy/chatgo/chat/foundation/logger"
+)
+
+func main() {
+	var log *logger.Logger
+
+	traceIDFn := func(ctx context.Context) string{
+		return "" // NEED TRACE 
+	}
+
+
+	log = logger.New(os.Stdout, slog.Level(logger.LevelDebug), "CAP", traceIDFn)
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil{
+		log.Error(ctx, "startup", "error",err)
+		os.Exit(1)
+	}
+}
+
+
+func run(ctx context.Context, log *logger.Logger) error{
+	// ------------------------------------------------------------
+	//GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	
+	return nil
+}
